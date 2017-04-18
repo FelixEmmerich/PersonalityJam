5ed4f54883855e6d760c4e1fc3d18d9f042c1683
Felix Emerich <felix.emerich@br-games.local>
2017-04-18 17:07:15 +0200

    Added DiscTurner class.

---
+++ PersonalityJam/Assets/Scripts/DiscTurner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscTurner : MonoBehaviour
+{
+
+    public Rigidbody2D Disc;
+    public float AngleAccelPerSecond;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Rotate right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Disc.angularVelocity -= AngleAccelPerSecond * Time.deltaTime;
+        }
+
+        //Rotate left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Disc.angularVelocity += AngleAccelPerSecond * Time.deltaTime;
+        }
+    }
+}
