3a22635b2d9e968d7f4e185b7a5e5926c7311ebd
Captain-C0ffee <se@sehrhardt.de>
2017-04-19 18:24:09 +0200

    mainly changes to Bubble script
    implemented a GameManager
    implemented a Loader for Manager Objects

--- PersonalityJam/Assets/Scripts/GameManager.cs
+++ PersonalityJam/Assets/Scripts/GameManager.cs
@@ -1,16 +1,75 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;  //static instance of GameManager to be visible by/accessible from any other script; initialize as null so there is none in the first place
+    enum GameState { playing, gameOver };
+    private GameState CurrentState = GameState.playing;
+    public int bubblesMax;
+    [SerializeField]
+    private int _bubbleCount = 0;
+    public GameObject[] bubbleCombo;
+
+    //===============================================================
+    //Getter / Setter
+    //===============================================================
+    
+    public int bubbleCount
+    {
+        get { return _bubbleCount; }
+        set { _bubbleCount += value; Debug.Log("BubbleCount: "+_bubbleCount); }
+    }
+
+    void Awake()
+    {
+        //check if instance already exists
+        if (instance == null)
+            //if not, set instance to this instance
+            instance = this;
+        //if instance already exists and it's not this instance:
+        else if (instance != this)
+            //then destroy this instance so there can only ever be one instance of our GameManager
+            Destroy(gameObject);
+        //sets this to not be destroyed when reloading the scene
+        DontDestroyOnLoad(gameObject);
+        bubblesMax = 150;
+    }
+
+
+
+    private void CheckGameState()
+    {
+        
+        switch (CurrentState)
+        {
+            case GameState.playing:
+                Debug.Log("The game is playing!");
+                break;
+            case GameState.gameOver:
+                Debug.Log("Gameover!");
+                break;
+            default:
+                Debug.Log("Nothing!");
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        if(bubbleCount >= bubblesMax)
+        {
+            CurrentState = GameState.gameOver;
+            Debug.Log("GameState changed to"+CurrentState);
+        }
+        CheckGameState();
+    }
 }
---
+++ PersonalityJam/Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;  //the GameManager prefab to instantiate
+    void Awake()
+    {
+        //check if a GameManager has already been assigned to static variable GameManager.instance
+        if (GameManager.instance == null)
+            //instantiate the gameManager prefab
+            Instantiate(gameManager);
+    }
+}
