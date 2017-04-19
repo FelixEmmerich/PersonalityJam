7559eb5f8af9542d370dd1213e98b3f1c641db84
Felix Emerich <felix.emerich@br-games.local>
2017-04-19 09:54:18 +0200

    Added rudimentary collision logic for FlyingObjects.

--- PersonalityJam/Assets/Scripts/FlyingObject.cs
+++ PersonalityJam/Assets/Scripts/FlyingObject.cs
@@ -6,15 +6,59 @@ public class FlyingObject : MonoBehaviour {
 
     public GameObject Target;
     public float Speed;
+    public int Connections = 0;
+    public int RequiredConnections;
+    public bool moving = false;
 
     private void Start()
     {
         Target = Disc.Instance.gameObject;
+        moving = true;
     }
 
     void Update()
     {
-        float step = Speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, step);
+        if (moving)
+        {
+            float step = Speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, step); 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Don't fire events on both FOs
+        if (!moving)
+        {
+            return;
+        }
+
+        //Collided with disc
+        if (other.gameObject==Target)
+        {
+            Debug.Log("Collided with disc");
+            moving = false;
+            return;
+        }
+
+        FlyingObject foComp = other.GetComponent<FlyingObject>();
+
+        //No collisions on two moving objects
+        if (foComp&&!foComp.moving)
+        {
+            moving = false;
+
+            //Todo: Tags
+            if (other.tag == tag)
+            {
+                foComp.CreateConnection();
+            }
+        }
+    }
+
+    public void CreateConnection()
+    {
+        Connections++;
+        //todo: Update connected FOs
     }
 }
